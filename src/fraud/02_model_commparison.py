60e68101306538bd4e4fa4afcb721b46eca4f8c9
Thorben Jensen <jensen.thorben@gmail.com>
2017-11-26 22:46:33 +0100

    changed h2o example to simple random forest; next: hyperparameter optimization.

--- src/fraud/02_model_commparison.py
+++ src/fraud/02_model_commparison.py
@@ -1,7 +1,7 @@
 # COMPARE PREDICITIVE POWER OF MODELS
 
 import h2o
-from h2o.automl import H2OAutoML
+from h2o.estimators import H2ORandomForestEstimator
 
 # connect to h2o
 h2o.init()
@@ -9,13 +9,13 @@ h2o.init()
 # load data
 file_path = 'data/PS_20174392719_1491204439457_log.csv'
 df = h2o.import_file(file_path, destination_frame='df')
-df = df.split_frame(ratios=[.1])[0]
-
-# inspect data
-df.describe()
 
 # train and validation split
-train, test, valid = df.split_frame(ratios=[.7, .15])
+df_sample = df.split_frame(ratios=[.1],
+                           destination_frames=['df_sample', 'null'])[0]
+train, valid = df_sample.split_frame(ratios=[.8],
+                                     destination_frames=['train', 'valid'])
+h2o.remove(['df', 'null'])
 
 # Identify predictors and response
 x = train.columns
@@ -25,10 +25,9 @@ x.remove('isFlaggedFraud')
 
 # For binary classification, response should be a factor
 train[y] = train[y].asfactor()
-test[y] = test[y].asfactor()
+valid[y] = valid[y].asfactor()
 
-# Run AutoML for 30 seconds
-aml = H2OAutoML(max_runtime_secs = 60*20)
-aml.train(x = x, y = y,
-          training_frame = train,
-          leaderboard_frame = test)
+# random forest
+model = H2ORandomForestEstimator(ntrees=100, max_depth=20, nfolds=5)
+model.train(x=x, y=y, training_frame=train, validation_frame=valid)
+# see localhost:54321 for validation metrics
